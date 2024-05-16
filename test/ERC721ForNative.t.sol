0e2f5b2e34abc078a511bc44d56c96571bae79a2
Arran Schlosberg <519948+ARR4N@users.noreply.github.com>
2024-05-16 14:42:06 +0100

    feat: `propose()` function (#11)
    * feat: `broadcast()` function
    
    * test: recovery of values from swap broadcast event
    
    * chore: run `forge snapshot`
    
    * test: standalone test of broadcast (not relying on `testHappyPath()`) + invert order of swap/salt in event
    
    The order of swap then salt in the broadcast event now matches the calls to `fill()` and `cancel()`. In theory
    (i.e not tested) this means that the logged `data` can be used as a call once prepended with the correct
    selector.
    
    * chore: rename `broadcast()` to `propose()`, change all references, and expand the function comment
    
    * chore: gas snapshot

--- test/ERC721ForNative.t.sol
+++ test/ERC721ForNative.t.sol
@@ -35,6 +35,16 @@ contract ERC721ForNativeTest is ERC721ForXTest, NativeTokenTest {
     }
 
     /// @inheritdoc ERC721ForXTest
+    function _propose(ERC721TestCase memory t) internal override returns (bytes32 salt, address swapper) {
+        return factory.propose(_asSwap(t));
+    }
+
+    /// @inheritdoc ERC721ForXTest
+    function _encodedSwapAndSalt(ERC721TestCase memory t, bytes32 salt) internal view override returns (bytes memory) {
+        return abi.encode(_asSwap(t), salt);
+    }
+
+    /// @inheritdoc ERC721ForXTest
     function _fill(ERC721TestCase memory t) internal override {
         _fill(t, t.base.native.callValue);
     }
