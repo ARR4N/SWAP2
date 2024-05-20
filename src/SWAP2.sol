5ba099888094627e543c57e96063a653d8e187ad
Arran Schlosberg <519948+ARR4N@users.noreply.github.com>
2024-05-20 11:57:55 +0100

    feat: configure platform fees as owner (#14)

--- src/SWAP2.sol
+++ src/SWAP2.sol
@@ -6,18 +6,27 @@ import {ERC721ForERC20SwapperDeployer} from "./ERC721ForERC20/ERC721ForERC20Swap
 import {MultiERC721ForNativeSwapperDeployer} from "./MultiERC721ForNative/MultiERC721ForNativeSwapperDeployer.gen.sol";
 import {MultiERC721ForERC20SwapperDeployer} from "./MultiERC721ForERC20/MultiERC721ForERC20SwapperDeployer.gen.sol";
 
+import {Ownable, Ownable2Step} from "@openzeppelin/contracts/access/Ownable2Step.sol";
+
 contract SWAP2 is
+    Ownable2Step,
     ERC721ForNativeSwapperDeployer,
     ERC721ForERC20SwapperDeployer,
     MultiERC721ForNativeSwapperDeployer,
     MultiERC721ForERC20SwapperDeployer
 {
+    constructor(address initialOwner) Ownable(initialOwner) {}
+
     struct PlatformFeeConfig {
         address payable recipient;
         uint16 basisPoints;
     }
 
     PlatformFeeConfig public feeConfig;
+
+    function setPlatformFee(address payable recipient, uint16 basisPoints) external onlyOwner {
+        feeConfig = PlatformFeeConfig({recipient: recipient, basisPoints: basisPoints});
+    }
 
     function _platformFeeConfig() internal view override returns (address payable, uint16) {
         PlatformFeeConfig memory config = feeConfig;
---
+++ test/Owner.t.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+import {Test} from "forge-std/Test.sol";
+import {console2} from "forge-std/console2.sol";
+
+import {SWAP2} from "../src/SWAP2.sol";
+
+import {Ownable} from "@openzeppelin/contracts/access/Ownable2Step.sol";
+
+contract OwnerTest is Test {
+    function testTransferOwnership(address initial, address newOwner) public {
+        // This doesn't need to be a comprehensive test, only a demonstration that we've correctly inherited from OZ.
+        vm.assume(initial != address(0));
+        vm.assume(newOwner != address(0));
+
+        SWAP2 s = new SWAP2(initial);
+
+        vm.prank(initial);
+        s.transferOwnership(newOwner);
+
+        assertEq(s.owner(), initial, "initial owner still owns");
+
+        vm.prank(newOwner);
+        s.acceptOwnership();
+
+        assertEq(s.owner(), newOwner, "new owner accepted");
+    }
+
+    function testPlatformFeeConfigOnlyOwner(address owner, address vandal, SWAP2.PlatformFeeConfig memory config)
+        public
+    {
+        vm.assume(owner != address(0));
+        vm.assume(owner != vandal);
+
+        SWAP2 s = new SWAP2(owner);
+
+        vm.expectRevert(abi.encodeWithSelector(Ownable.OwnableUnauthorizedAccount.selector, vandal));
+        vm.prank(vandal);
+        s.setPlatformFee(config.recipient, config.basisPoints);
+    }
+}
