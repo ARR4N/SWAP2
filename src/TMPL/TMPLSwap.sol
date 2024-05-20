c26349a745fdb91d90a4a8be8bb4967db81a21a4
Arran Schlosberg <519948+ARR4N@users.noreply.github.com>
2024-05-20 11:29:04 +0100

    Generate `Swapper` contracts (#13)
    * chore: generate `XForNativeSwapper`s
    
    * chore: delete  contracts as they're generated and git-ignored
    
    * chore: generate `XForERC20Swapper`s

--- src/TMPL/TMPLSwap.sol
+++ src/TMPL/TMPLSwap.sol
@@ -1,11 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.25;
 
+import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
 import {Consideration, Parties} from "../TypesAndConstants.sol";
 import {ERC721Token} from "../ERC721SwapperLib.sol";
 
 struct TMPLSwap {
-    Parties parties; // Can be substituted for PayableParties
+    Parties parties; // MUST be substituted for PayableParties for native-token consideration
     ERC721Token offer; // Can be substituted for any assets
     Consideration consideration;
+    IERC20 currency; // SHOULD be removed for native-token consideration
 }
