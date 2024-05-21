777a3029dfa41efbf8b11be3ad37ea75f6487d64
Arran Schlosberg <me@arranschlosberg.com>
2024-05-21 16:11:02 +0100

    doc: post-execution invariant guarantees with native-token consideration

--- src/NativeTokenConsideration.sol
+++ src/NativeTokenConsideration.sol
@@ -36,14 +36,23 @@ contract NativeTokenConsideration {
         for (uint256 i = 0; i < tP.length; ++i) {
             payable(tP[i].to).sendValue(tP[i].amount);
         }
+
+        // MUST remain as the last step to guarantee that _postExecutionInvariantsMet() returns true. This means that
+        // the only actor capable of griefing the invariants is the seller (by returning some of these funds), which
+        // would only act to harm themselves.
         _sendEntireBalance(parties.seller);
     }
 
     /// @notice Sends the contract's entire balance to `parties.buyer`.
     function _cancel(PayableParties memory parties) internal virtual {
+        // MUST remain as the last step for the same reason as _disburseFunds().
         _sendEntireBalance(parties.buyer);
     }
 
+    /**
+     * @dev Sends the entirety of the contract's balance to the specified address. As this is called as the final step
+     * in all paths, the post-execution invariant of a zero balance is ensured.
+     */
     function _sendEntireBalance(address payable to) private {
         to.sendValue(address(this).balance);
     }
