c4c1d932c9f97cbeffac205ec4ed0d5bb257901f
Arran Schlosberg <me@arranschlosberg.com>
2024-05-21 17:12:07 +0100

    test: explicit demonstration of gas improvement

--- test/ERC721TransferLib.t.sol
+++ test/ERC721TransferLib.t.sol
@@ -17,10 +17,68 @@ contract ERC721TransferLibTest is Test, ITestEvents {
     uint256 constant TOKENS_PER_CONTRACT = 5;
 
     function testMultiERC721TokenTransfer(
-        address[NUM_CONTRACTS] calldata contracts,
+        bytes32[NUM_CONTRACTS] calldata deploySalts,
         uint256[NUM_CONTRACTS][TOKENS_PER_CONTRACT] calldata ids,
         Parties memory parties
     ) public {
+        ERC721TransferLib.MultiERC721Token[] memory tokens = _testSetup(deploySalts, ids, parties);
+
+        _assertOwner(tokens, parties.seller);
+
+        vm.startPrank(parties.seller);
+        ERC721TransferLib._transfer(tokens, parties);
+        vm.stopPrank();
+
+        _assertOwner(tokens, parties.buyer);
+    }
+
+    function testGas(
+        bytes32[NUM_CONTRACTS] calldata deploySalts,
+        uint256[NUM_CONTRACTS][TOKENS_PER_CONTRACT] calldata ids,
+        Parties memory parties
+    ) public {
+        ERC721TransferLib.MultiERC721Token[] memory tokens = _testSetup(deploySalts, ids, parties);
+
+        vm.startPrank(parties.seller);
+        uint256 naiveGas;
+        uint256 libGas;
+
+        {
+            uint256 snap = vm.snapshot();
+            naiveGas = gasleft();
+
+            for (uint256 i = 0; i < tokens.length; ++i) {
+                IERC721 t = tokens[i].addr;
+                for (uint256 j = 0; j < tokens[i].ids.length; ++j) {
+                    t.transferFrom(parties.seller, parties.buyer, tokens[i].ids[j]);
+                }
+            }
+
+            naiveGas -= gasleft();
+
+            _assertOwner(tokens, parties.buyer);
+            vm.revertTo(snap);
+        }
+
+        {
+            libGas = gasleft();
+            ERC721TransferLib._transfer(tokens, parties);
+            libGas -= gasleft();
+        }
+
+        // 140 gas per token was found empirically; it has no special meaning other than to demonstrate the gas saving
+        // in this particular instance and may be a change-detector test.
+        assertLt(libGas + 140 * NUM_CONTRACTS * TOKENS_PER_CONTRACT, naiveGas);
+        console2.log(naiveGas - libGas);
+    }
+
+    mapping(bytes32 => bool) private _saltSeen;
+
+    function _testSetup(
+        bytes32[NUM_CONTRACTS] calldata deploySalts,
+        uint256[NUM_CONTRACTS][TOKENS_PER_CONTRACT] calldata ids,
+        Parties memory parties
+    ) internal returns (ERC721TransferLib.MultiERC721Token[] memory) {
         vm.assume(parties.seller != parties.buyer);
         vm.assume(parties.seller != address(0));
         vm.assume(parties.buyer != address(0));
@@ -31,15 +89,14 @@ contract ERC721TransferLibTest is Test, ITestEvents {
         ERC721TransferLib.MultiERC721Token[] memory tokens = new ERC721TransferLib.MultiERC721Token[](NUM_CONTRACTS);
 
         for (uint256 i = 0; i < NUM_CONTRACTS; ++i) {
-            address a = contracts[i];
-            vm.assume(uint160(a) > 0x0a);
-            vm.assume(a.code.length == 0);
-            vm.etch(a, tokenTemplate.code);
+            bytes32 salt = deploySalts[i];
+            vm.assume(!_saltSeen[salt]);
+            _saltSeen[salt] = true;
 
-            tokens[i].addr = IERC721(a);
+            Token t = new Token{salt: deploySalts[i]}();
+            tokens[i].addr = t;
             tokens[i].ids = new uint256[](ids[i].length);
 
-            Token t = Token(a);
             for (uint256 j = 0; j < TOKENS_PER_CONTRACT; ++j) {
                 uint256 id = ids[i][j];
                 vm.assume(!t.exists(id));
@@ -49,13 +106,7 @@ contract ERC721TransferLibTest is Test, ITestEvents {
             }
         }
 
-        _assertOwner(tokens, parties.seller);
-
-        vm.startPrank(parties.seller);
-        ERC721TransferLib._transfer(tokens, parties);
-        vm.stopPrank();
-
-        _assertOwner(tokens, parties.buyer);
+        return tokens;
     }
 
     function _assertOwner(ERC721TransferLib.MultiERC721Token[] memory tokens, address owner) internal {
