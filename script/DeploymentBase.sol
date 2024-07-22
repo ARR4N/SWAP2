ec5ebae4b28c7005ad734533c5e758af5aaf5c49
Arran Schlosberg <me@arranschlosberg.com>
2024-07-22 20:27:34 +0100

    feat: `deploy` Foundry profile with deploy-script checking

---
+++ script/DeploymentBase.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+// Copyright 2024 Divergence Tech Ltd.
+pragma solidity ^0.8.13;
+
+import {Script, console} from "forge-std/Script.sol";
+
+/**
+ * @notice Deployment script that confirms the current Foundry profile in setup.
+ * @dev Useful for turning off features during development (e.g. slow `via_ir`) that MUST be enabled for deployment.
+ */
+contract DeploymentBase is Script {
+    error NotDeployProfile(string);
+
+    function setUp() public view virtual {
+        string memory profile = vm.envString("FOUNDRY_PROFILE");
+        if (keccak256(abi.encode(profile)) != keccak256("deploy")) {
+            revert NotDeployProfile(profile);
+        }
+    }
+}
